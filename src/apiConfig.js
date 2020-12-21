045a99d2841691229f01ffce2d3f6957febe228d
Waisath-CJ <curt.waisath.jr@outlook.com>
2020-12-21 11:53:38 -0700

    Update apiConfig File
    - Updated the `apiConfig` to have the correct url to the deployed API

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://fierce-waters-36807.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
